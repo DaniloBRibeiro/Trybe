2b036342b9f9ea8b9a96d9c33418612720184e6b
Danilo de Barros Ribeiro <nilo22@gmail.com>
2022-05-24 17:38:51 -0300

    atualizando

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -43,7 +43,7 @@ console.log(newEmployees(nameEmployee));
 
 const lottery = (myNumber, callback) => {
   const number = Math.floor((Math.random()* 5)+ 1);
-  
+
   return callback(myNumber, number) ? 'Lucky' : 'Try again';
 };
 
